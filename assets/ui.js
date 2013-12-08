6bf5b6c05103e7e717c5c896c475425f6c86b7a0
Andrew Monks <a@monks.co>
2013-12-07 18:57:55 -0600

    grouping posts in menu

--- assets/ui.js
+++ assets/ui.js
@@ -1,5 +1,23 @@
+// sort nav by class
+
+// start by declaring an order for the options
 var array = ['rpi', 'web'];
 
+// then put the elements into that order
 $.each(array,function(index,value){
    $('.nav').append($('.'+value));
+});
+
+
+
+// group nav classes and add headers
+
+$(".nav li").each(function() {
+    var group = $(this).children("span").class;
+    // If the grouping <li> doesn't exist, create it
+    if ($(".nav li.group." + group).length === 0) {
+        $(".nav").append($('<li class="group ' + group + '">' + group + '<ul></ul></li>'));
+    }
+    // Add the current <li> to the corresponding grouping <li>
+    $(this).appendTo($(".nav li.group." + group + " ul"));
 });
