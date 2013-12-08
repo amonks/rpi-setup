bc07a910aefcf3848340b65fcf1ac4d5bc888544
Andrew Monks <a@monks.co>
2013-12-07 19:05:11 -0600

    grouping posts in menu

--- assets/ui.js
+++ assets/ui.js
@@ -16,8 +16,8 @@ $(".nav li").each(function() {
     var group = $(this).attr('class');
     // If the grouping <li> doesn't exist, create it
     if ($(".nav li.group." + group).length === 0) {
-        $(".nav").append($('<li class="group ' + group + '">' + group + '<ul></ul></li>'));
+        $(".nav").append($('<ul class="group"> ' + group + '">' + group + '<ul></ul></li>'));
     }
     // Add the current <li> to the corresponding grouping <li>
-    $(this).appendTo($(".nav li.group." + group + " ul"));
+    $(this).appendTo($(".nav ul.group." + group + " li"));
 });
