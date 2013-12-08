4cc99be9970a3bc89ce7474c588a9f5d083c2e5b
Andrew Monks <a@monks.co>
2013-12-08 16:07:58 -0600

    pretty printing code blocks

--- assets/ui.js
+++ assets/ui.js
@@ -61,4 +61,4 @@ $("li:contains('" + document.title + "')").addClass("active")
 
 // turn on pretty printing for code blocks
 
-$('pre').addClass('prettyprint');
+$('pre').addClass('prettyprint linenums');
