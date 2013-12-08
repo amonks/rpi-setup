e74f8a543facec6f10858a2759a9102324350640
Andrew Monks <a@monks.co>
2013-12-07 18:41:34 -0600

    grouping posts in menu

--- assets/ui.js
+++ assets/ui.js
@@ -1,3 +1,14 @@
-$('.nav li').sort(function(a,b){
-   return a.dataset.sid > b.dataset.sid
-}).appendTo('.clist')
+sort(postLink, class)
+
+
+
+function sort(sortProperty, sortClass)
+{
+    var sortThings = document.getElementsByClassName(sortClass);
+    sortThings = Array.prototype.slice.call(sortThings,0)
+    sortThings.sort(function(a,b)
+         {
+             return (a[sortProperty] > b[sortProperty]);
+         })
+    return sortThings;
+}
