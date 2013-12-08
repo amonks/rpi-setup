b303736855897a55399179b2affddb32ad3a897a
Andrew Monks <a@monks.co>
2013-12-07 20:10:57 -0600

    js

--- assets/ui.js
+++ assets/ui.js
@@ -30,7 +30,7 @@ $('.topic').each(function() {
         container.insertBefore(collection[0]);
         for(i=0;i<collection.length;i++)
         {
-            collection[i].appendTo($('.nav .' + thisClass));
+            collection[i].appendTo($('.nav').filter('.' + thisClass));
         }
         collection = [];
     }
