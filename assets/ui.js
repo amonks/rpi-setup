480a12a23fae074f6cc663d2360ad670f33991a8
Andrew Monks <a@monks.co>
2013-12-07 18:49:25 -0600

    grouping posts in menu

--- assets/ui.js
+++ assets/ui.js
@@ -1,14 +1,5 @@
-sort("postLink", "class")
+var array = ['rpi', 'web'];
 
-
-
-function sort(sortProperty, sortClass)
-{
-    var sortThings = document.getElementsByClassName(sortClass);
-    sortThings = Array.prototype.slice.call(sortThings,0)
-    sortThings.sort(function(a,b)
-         {
-             return (a[sortProperty] > b[sortProperty]);
-         })
-    return sortThings;
-}
+$.each(array,function(index,value){
+   $('.nav').append($('.'+value));
+});
