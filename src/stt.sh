c99b584fb8d1b03b8e7083f4f26c24b8ab8e05d9
Andrew Monks <a@monks.co>
2013-12-09 21:56:52 -0600

    stt output directory

--- src/stt.sh
+++ src/stt.sh
@@ -4,15 +4,11 @@
 #modified by Andrew Monks
 
 echo "Recording your Speech (Ctrl+C to Transcribe)"
-arecord -D plughw:0,0 --duration=5 -f cd -t wav -d 0 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o stt-recording.flac;
+arecord -D plughw:0,0 --duration=5 -f cd -t wav -d 0 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o stt-output/stt-recording.flac;
 
 echo "Converting Speech to Text..."
-wget -q -U "Mozilla/5.0" --post-file stt-recording.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12 > stt-text.txt
+wget -q -U "Mozilla/5.0" --post-file stt-output/stt-recording.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12 > stt-output/stt-text.txt
 
 echo "You Said:"
-value=`cat stt-text.txt`
-<<<<<<< HEAD
+value=`cat stt-output/stt-text.txt`
 echo "$value"
-=======
-echo "$value"
->>>>>>> 5a4ff24810a5c8b4a5f156a256a4d92809a26390
