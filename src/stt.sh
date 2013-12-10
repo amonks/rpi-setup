a93244b4847c9424aa3b83a8e0d1bff1744a2da9
Andrew Monks <a@monks.co>
2013-12-09 22:45:51 -0600

    flac

--- src/stt.sh
+++ src/stt.sh
@@ -6,11 +6,14 @@
 mkdir -p stt-output
 
 echo "Recording your Speech (Ctrl+C to Transcribe)"
-arecord -D plughw:0,0 --duration=5 -f cd -t wav -d 0 -q -r 16000 | flac - -s -f --best --sample-rate 16000 -o stt-output/stt-recording.flac &
+arecord -D plughw:0,0 --duration=5 -f cd -t wav -d 0 -q -r 16000 stt-output/stt-recording.wav &
 PID=$!
 sleep 5s
 kill $PID
 sleep 1s
+
+echo "Converting speech to FLAC"
+flac - -s -f --best --sample-rate 16000 -o stt-output/stt-recording.flac
 
 echo "Converting Speech to Text..."
 wget -q -U "Mozilla/5.0" --post-file stt-output/stt-recording.flac --header "Content-Type: audio/x-flac; rate=16000" -O - "http://www.google.com/speech-api/v1/recognize?lang=en-us&client=chromium" | cut -d\" -f12 > stt-output/stt-text.txt
